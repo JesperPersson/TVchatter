33c12ac0dbc80dcc83de30e3a7eff4b519495e8c
jper <ucanreachjesper@gmail.com>
2015-09-17 14:26:31 +0200

    implemented moare code

--- app/src/main/java/com/kladdle/tvchatter/DetailView.java
+++ app/src/main/java/com/kladdle/tvchatter/DetailView.java
@@ -2,14 +2,33 @@ package com.kladdle.tvchatter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
+import com.nineoldandroids.view.ViewHelper;
 import com.squareup.picasso.Picasso;
 
-public class DetailView extends AppCompatActivity {
+public class DetailView extends AppCompatActivity implements ObservableScrollViewCallbacks {
+    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
+
+    private View mImageView;
+    private View mOverlayView;
+    private ObservableScrollView mScrollView;
+    private TextView mTitleView;
+
+    private int mActionBarSize;
+
+    private int mFlexibleSpaceImageHeight;
 
 
     @Override
@@ -18,7 +37,16 @@ public class DetailView extends AppCompatActivity {
         setContentView(R.layout.activity_detail_view);
 
 
+        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+        mActionBarSize = 10;
 
+        mImageView = findViewById(R.id.backdrop);
+        mOverlayView = findViewById(R.id.overlay);
+        mScrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        mScrollView.setScrollViewCallbacks(this);
+        mTitleView = (TextView) findViewById(R.id.title);
+        mTitleView.setText(getTitle());
+        setTitle(null);
 
 
         //display image
@@ -27,6 +55,71 @@ public class DetailView extends AppCompatActivity {
         ImageView backdropView = (ImageView) findViewById(R.id.backdrop);
         Picasso.with(getApplicationContext()).load("http://image.tmdb.org/t/p/w500/"+message).into(backdropView);
 
+        ScrollUtils.addOnGlobalLayoutListener(mScrollView, new Runnable() {
+            @Override
+            public void run() {
+
+                mScrollView.scrollTo(0, mFlexibleSpaceImageHeight - mActionBarSize);
+
+                // If you'd like to start from scrollY == 0, don't write like this:
+                //mScrollView.scrollTo(0, 0);
+                // The initial scrollY is 0, so it won't invoke onScrollChanged().
+                // To do this, use the following:
+                onScrollChanged(0, false, false);
+
+                // You can also achieve it with the following codes.
+                // This causes scroll change from 1 to 0.
+                mScrollView.scrollTo(0, 1);
+                mScrollView.scrollTo(0, 0);
+            }
+        });
+
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+
+        Log.v("scrolling", "nu");
+
+        // Translate overlay and image
+        float flexibleRange = mFlexibleSpaceImageHeight - mActionBarSize;
+        int minOverlayTransitionY = mActionBarSize - mOverlayView.getHeight();
+        ViewHelper.setTranslationY(mOverlayView, ScrollUtils.getFloat(-scrollY, minOverlayTransitionY, 0));
+        ViewHelper.setTranslationY(mImageView, ScrollUtils.getFloat(-scrollY / 2, minOverlayTransitionY, 0));
+
+        // Change alpha of overlay
+        ViewHelper.setAlpha(mOverlayView, ScrollUtils.getFloat((float) scrollY / flexibleRange, 0, 1));
+
+        // Scale title text
+        float scale = 1 + ScrollUtils.getFloat((flexibleRange - scrollY) / flexibleRange, 0, MAX_TEXT_SCALE_DELTA);
+        ViewHelper.setPivotX(mTitleView, 0);
+        ViewHelper.setPivotY(mTitleView, 0);
+        ViewHelper.setScaleX(mTitleView, scale);
+        ViewHelper.setScaleY(mTitleView, scale);
+
+        // Translate title text
+        int maxTitleTranslationY = (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale);
+        int titleTranslationY = maxTitleTranslationY - scrollY;
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        ActionBar ab = getSupportActionBar();
+        if (scrollState == ScrollState.UP) {
+            if (ab.isShowing()) {
+                ab.hide();
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (!ab.isShowing()) {
+                ab.show();
+            }
+        }
     }
 
     @Override
